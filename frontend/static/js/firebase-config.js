a25ee731e8cfe2cfe8f8785565a6f6bebc700f45
André Souza <andregomes.vs@gmail.com>
2026-07-28 00:22:15 -0300

    config firebase

--- frontend/static/js/firebase-config.js
+++ frontend/static/js/firebase-config.js
@@ -4,12 +4,13 @@
 // Cole aqui os valores do MESMO projeto usado na agenda consolidada.
 // ============================================================
 export const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "seu-projeto.firebaseapp.com",
-  projectId: "seu-projeto",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:xxxxxxxxxxxx",
+  apiKey: "AIzaSyAmy89a5r8kHxLxussQE4AYUgLcCj9JFQw",
+  authDomain: "calendar-190c7.firebaseapp.com",
+  projectId: "calendar-190c7",
+  storageBucket: "calendar-190c7.firebasestorage.app",
+  messagingSenderId: "41890555797",
+  appId: "1:41890555797:web:e1727dd5aa628bff0ab89c",
+  measurementId: "G-09SRK20JTZ",
 };
 
 // Base da API do backend. Vazio = mesma origem (o Flask serve o frontend),
